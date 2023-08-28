589666511bc7647053d1e3419b4b152b35026968
Antonio Silva <antoniofranciscosilva.sp@gmail.com>
2023-08-27 23:48:46 -0300

    Primeira adição

---
+++ WebCoreTDD/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e5c031f7-279d-4ecf-8b02-2d8f45d14503")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebCoreTDD/WebCoreTDD.Tests/FibonacciTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCoreTDD.Tests
+{
+    [TestClass]
+    public class FibonacciTest
+    {
+        //POSIÇÃO   0   1   2   3   4   5   6   7   8   9   10  ...
+        //NUMERO    0   1   1   2   3   5   8   13  21  34  55  ...
+
+        [TestMethod]
+        public void PrimeiroElementoDeveSerZero()
+        {
+            var resultado = Fibonacci.ObterElemento(0);
+            Assert.AreEqual(resultado, 0, "Valor não é zero");
+        }
+
+        // Para fins de teste vamos criar aqui mesmo, mas o correto seria no projeto
+        public static class Fibonacci
+        {
+            public static int ObterElemento(int posicao)
+            {
+                return 0;
+            }
+        }
+    }
+}
